de8ff00d81dc8d9197798e562665d0f9951222b2
fmf06 <fmf06pta@gmail.com>
2022-06-28 15:42:56 +0300

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Authentication aut = new Authentication();
         var user = aut.verificationLogin();
-        user.mainMenu();
+         user.mainMenu();
     }
 }
